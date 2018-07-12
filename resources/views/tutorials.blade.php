d12fa85ca426c4e8ed047808119befa3de778608
goosza <vellenore@gmail.com>
2018-07-12 15:12:42 +0900

    editing

--- resources/views/tutorials.blade.php
+++ resources/views/tutorials.blade.php
@@ -1,1 +1,18 @@
 @extends("layout")
+@section("content")
+    <p>
+      
+      <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#multiCollapseExample2" aria-expanded="false" aria-controls="multiCollapseExample2">Toggle second element</button>
+      
+    </p>
+    <div class="row">
+      <div class="col">
+        <div class="collapse multi-collapse" id="multiCollapseExample1">
+          <div class="card card-body">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
+          </div>
+        </div>
+      </div>
+      
+    </div>
+@endsection
