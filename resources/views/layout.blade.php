3b5ce147a9e0ca4a6ca22c2fb78f3532c4ce5ba1
goosza <vellenore@gmail.com>
2018-07-12 10:50:36 +0900

    t

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,9 +19,10 @@
         <nav class="navbar navbar-light bg-light" >
           
           <form class="form-inline" style="margin-left:38%;">
+            <img src="#" class="menu">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" >
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            <img src="..." alt="..." class="rounded-circle">
+            <img src="" alt="..." class="rounded-circle">
           </form>
         </nav>
     </body>
